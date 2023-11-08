a7568f5c0f1a691624ed8cd4cd1c58ce8b27b504
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-11-08 11:03:16 -0500

    use hidden files in telescope

--- nvim/.config/nvim/after/plugin/telescope.lua
+++ nvim/.config/nvim/after/plugin/telescope.lua
@@ -2,31 +2,36 @@ local telescope = require("telescope")
 local actions = require("telescope.actions")
 
 telescope.setup({
-	defaults = {
-		file_ignore_patterns = { ".git/", ".cache", "%.pdf", "%.mkv", "%.mp4", "%.zip", "node_modules/" },
-		mappings = {
-			i = {
-				["<C-k>"] = actions.move_selection_previous, -- move to prev result
-				["<C-j>"] = actions.move_selection_next, -- move to next result
-				["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
-			},
-		},
-	},
-	extensions = {
-		file_browser = {
-			hijack_netrw = true,
-		},
-	},
+    defaults = {
+        file_ignore_patterns = { ".git/", ".cache", "%.pdf", "%.mkv", "%.mp4", "%.zip", "node_modules/" },
+        mappings = {
+            i = {
+                ["<C-k>"] = actions.move_selection_previous,                       -- move to prev result
+                ["<C-j>"] = actions.move_selection_next,                           -- move to next result
+                ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist, -- send selected to quickfixlist
+            },
+        },
+    },
+    pickers = {
+        find_files = {
+            hidden = true
+        }
+    },
+    extensions = {
+        file_browser = {
+            hijack_netrw = true,
+        },
+    },
 })
 
 telescope.load_extension("fzy_native")
 telescope.load_extension("file_browser")
 
 vim.api.nvim_set_keymap(
-	"n",
-	"<leader>fb",
-	":Telescope file_browser path=%:p:h select_buffer=true<CR>",
-	{ noremap = true }
+    "n",
+    "<leader>fb",
+    ":Telescope file_browser path=%:p:h select_buffer=true<CR>",
+    { noremap = true }
 )
 
 local builtin = require("telescope.builtin")
