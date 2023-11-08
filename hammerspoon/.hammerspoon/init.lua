63083bdef0183b44d0727e548e63d8ff14e34402
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-11-08 11:03:00 -0500

    use vim mouse movements

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -4,6 +4,7 @@ local LeftRightHotkey = require("LeftRightHotkey")
 
 require("apps").start()
 require("windows").start()
+require('mouse')('cmd', 'm')
 
 -- General config
 LeftRightHotkey.bind({ 'lCmd', 'lCtrl' }, 'r', function() hs.reload() end)
