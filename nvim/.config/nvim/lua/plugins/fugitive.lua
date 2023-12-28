b926b88cb0d94951f22d35a8cd64e48f24abafe9
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2023-12-28 17:28:10 -0500

    add <cmd>

--- nvim/.config/nvim/lua/plugins/fugitive.lua
+++ nvim/.config/nvim/lua/plugins/fugitive.lua
@@ -3,6 +3,6 @@ return {
     event = "VeryLazy",
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git, {})
-        vim.keymap.set("n", "<leader>gp", ":Git push<CR>", {})
+        vim.keymap.set("n", "<leader>gp", "<cmd>Git push<CR>", {})
     end,
 }
