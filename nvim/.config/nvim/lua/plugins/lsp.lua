0b4121c32f2789c9ee7c0e06b7fdf6d2c01ac177
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2024-05-28 11:06:23 -0400

    lsp changes

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -2,11 +2,12 @@ return {
     {
         "neovim/nvim-lspconfig",
         dependencies = {
-            "williamboman/mason-lspconfig.nvim",
             "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
         },
         event = "VeryLazy",
         config = function()
+            local mason_lspconfig = require("mason-lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local on_attach = function(_, bufnr)
                 local opts = { noremap = true, silent = true, buffer = bufnr }
@@ -19,32 +20,52 @@ return {
                 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)      -- go to previous diagnostic
                 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)      -- go to next diagnostic
                 vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)              -- show hover info
+                vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts) -- show code actions
                 vim.keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts)     -- restart lsp server
             end
+
             require("mason").setup()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "tsserver", "gopls", "svelte", "html", "jsonls" },
+            mason_lspconfig.setup({
+                ensure_installed = { "lua_ls", "tsserver", "gopls", "svelte", "html", "jsonls", "templ" },
             })
-            require("mason-lspconfig").setup_handlers({
+            mason_lspconfig.setup_handlers({
                 function(server_name)
-                    require("lspconfig")[server_name].setup({
+                    local options = {
                         on_attach = on_attach,
                         capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                diagnostics = { globals = { "vim" } },
-                            },
-                        },
-                    })
+                    }
+                    if server_name == "html" then
+                        options.filetypes = { "html", "templ" }
+                    end
+                    if server_name == "htmx" then
+                        options.filetypes = { "html", "templ" }
+                    end
+                    if server_name == "tailwindcss" then
+                        options.filetypes = { "templ", "astro", "javascript", "typescript", "react", "svelte", "html" }
+                        options.init_options = { userLanguages = { templ = "html" } }
+                    end
+
+                    require("lspconfig")[server_name].setup(options)
                 end,
             })
-
             vim.api.nvim_create_autocmd("BufWritePre", {
                 pattern = { "*" },
                 callback = function()
+                    if vim.bo.filetype == "templ" then
+                        vim.lsp.buf.format({
+                            async = true,
+                            name = "templ",
+                            filter = function(client)
+                                return client.name == "templ"
+                            end,
+                        })
+                        return
+                    end
                     vim.lsp.buf.format()
                 end,
             })
+
+            vim.filetype.add({ extension = { templ = "templ" } })
         end,
     },
 }
