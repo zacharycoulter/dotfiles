9d4fc9c0d509439d5436fbaf4ff883db4dd0da9c
Zachary Coulter <8799143+zacharycoulter@users.noreply.github.com>
2024-05-28 11:07:20 -0400

    treesitter not lazy

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,15 +1,41 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "VeryLazy",
-    config = function()
-        local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter-context", config = true },
+	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-            auto_install = true,
-            highlight = { enabled = true },
-            indent = { enabled = true },
-        })
-    end,
+		configs.setup({
+			sync_install = false,
+			auto_install = true,
+			indent = {
+				enable = true,
+			},
+			highlight = {
+				enable = true,
+			},
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"svelte",
+				"json",
+				"javascript",
+				"typescript",
+				"tsx",
+				"yaml",
+				"html",
+				"css",
+				"markdown",
+				"graphql",
+				"bash",
+				"dockerfile",
+				"gitignore",
+			},
+		})
+	end,
 }
